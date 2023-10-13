f2c2a3fa7496c7bba127c16fa20e2e8e615c0fd3
@hexlet/cli <support@hexlet.io>
2023-10-13 22:21:38 +0300

    submit java-spring-ru/first-start

---
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/about")
+    String about() {
+        return "Welcome to Hexlet!";
+    }
+}
